dbf46822964a7e798f6c43ffe2794c16b27dd721
Keeper991 <nesmile23@gmail.com>
2020-11-18 01:42:12 +0900

    update Section style

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -2,18 +2,29 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-const Container = styled.div``;
+const Container = styled.div`
+  :not(:last-child) {
+    margin-bottom: 50px;
+  }
+`;
 
-const Title = styled.span``;
+const Title = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+`;
 
-const Grid = styled.div``;
+const Grid = styled.div`
+  margin-top: 25px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 125px);
+`;
 
-const Section = ({ title, children }) => {
+const Section = ({ title, children }) => (
   <Container>
     <Title>{title}</Title>
     <Grid>{children}</Grid>
-  </Container>;
-};
+  </Container>
+);
 
 Section.propTypes = {
   title: PropTypes.string,
